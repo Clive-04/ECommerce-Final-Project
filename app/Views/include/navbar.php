285189e37132f0e4d288b951e7c96c770fc108d2
Ozbert <202310393@fit.edu.ph>
2026-03-21 20:04:11 +0800

    switch

--- app/Views/include/navbar.php
+++ app/Views/include/navbar.php
@@ -22,11 +22,11 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url('/about') ?>">About</a>
+                    <a class="nav-link" href="<?= base_url('/cart') ?>">Cart</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="<?= base_url('/cart') ?>">Cart</a>
+                    <a class="nav-link" href="<?= base_url('/about') ?>">About</a>
                 </li>
 
                 <?php if (session()->get('role') == 'admin'): ?>
